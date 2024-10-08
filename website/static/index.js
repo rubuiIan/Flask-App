cc875b25fc2f308a81993758e7e6e1ad9dc4a29c
ANGELA KARIUKI <iyanrubui@gmail.com>
2024-10-08 14:57:36 +0300

    final commit

---
+++ website/static/index.js
@@ -0,0 +1,12 @@
+function deleteNote(noteId) {
+    fetch("/delete-note", {
+      method: "POST",
+      body: JSON.stringify({ noteId: noteId }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((_res) => {
+      window.location.href = "/";
+    });
+  }
+  
